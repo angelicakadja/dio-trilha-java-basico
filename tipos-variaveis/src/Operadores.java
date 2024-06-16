22069d5f668b417cc85096da553a6137ef5ac3f7
angelica.kadja <angelicakadja@gmail.com>
2024-06-16 19:44:44 -0300

    Experimentos de variáveis com Boolean e Equals

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,131 @@
+public class Operadores {
+    public static void main (String[] args) {
+       
+        // String unida com operador +
+
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+            System.out.println(nomeCompleto);
+        
+        // Concatenação com String e Número
+
+        String concatenacao = "?"; 
+
+            concatenacao = 1+1+1+"1";
+                System.out.println(concatenacao);
+
+            concatenacao = 1+"1"+1+1;
+                System.out.println(concatenacao);
+
+            concatenacao = 1+"1"+1+"1";
+                System.out.println(concatenacao);
+
+            concatenacao = "1"+1+1+1;
+                System.out.println(concatenacao);
+
+            concatenacao = "1"+(1+1+1);
+                System.out.println(concatenacao);
+
+            int numero = 5;
+                System.out.println(numero);
+
+        // Repetição
+
+        numero = numero + 2;
+            System.out.println(numero);
+
+        // Decrementar automático
+
+        System.out.println(numero --);
+        System.out.println(numero);
+
+        // Invertendo variável do tipo Booleana
+
+        boolean variavel = true;
+
+            variavel = !variavel;
+                System.out.println(variavel);
+
+        // Operador Ternário - método 1
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = "";
+
+        if (a==b)
+            resultado = "verdadeiro";
+
+        else
+            resultado = "falso";
+
+        System.out.println(resultado);
+
+        // Operador Ternário - método 2
+
+        int c, d;
+
+        c = 5;
+        d = 6;
+
+        String valor = c==d ?"1" : "0";
+
+        System.out.println(valor);
+
+        // Operadores Relacionais
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+	        System.out.println("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+	        System.out.println("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+	        System.out.println("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+	        System.out.println("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+	        System.out.println("Numero 1 é diferente de numero 2");
+
+        // Operadores Relacionais com Boolean numeral
+
+        int numero3 = 2;
+        int numero4 = 5;
+
+        boolean simNao = numero3 == numero4;
+
+            if(numero3 < numero4){
+                System.out.println("a nossa condição é verdadeira");
+            }
+
+            System.out.println("numero3 é igual a numero4? " + simNao);
+        
+        simNao = numero3 != numero4;
+            System.out.println("numero3 é diferente de numero4? " + simNao);
+
+        simNao = numero3 > numero4;
+            System.out.println("numero3 é maior que numero4? " + simNao);
+
+        // Comparar Objetos com Boolean String
+
+        String nomeUm = "Kadja";
+        String nomeDois = "Angelica";
+        String nomeTres = new String("Kadja");
+        
+        System.out.println(nomeUm == nomeDois);
+
+        System.out.println(nomeUm != nomeDois);
+
+        // Comparar objetos com equals String
+
+        System.out.println(nomeUm.equals(nomeTres));
+    }
+        
+
+    }
