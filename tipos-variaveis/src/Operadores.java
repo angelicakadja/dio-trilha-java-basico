9e1c3bedc611fb7fca8800982baa158ccee8bd8d
angelica.kadja <angelicakadja@gmail.com>
2024-06-19 17:06:00 -0300

    Inserindo outras codificações como operadores lógicos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -125,6 +125,22 @@ public class Operadores {
         // Comparar objetos com equals String
 
         System.out.println(nomeUm.equals(nomeTres));
+
+        // Operadores Lógicos
+
+        boolean condicao1=true;
+        boolean condicao2=false;
+
+        if(condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+        }
+            System.out.println("fim");
+
+        if(condicao1 || condicao2){
+            System.out.println("uma das condições é verdadeira");
+        }
+        
+
     }
         
 
