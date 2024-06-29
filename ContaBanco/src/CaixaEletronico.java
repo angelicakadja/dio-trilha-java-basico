1c5c66f0097b9df8466c4a2a3bdcf9a68f707190
angelica.kadja <angelicakadja@gmail.com>
2024-06-29 16:02:04 -0300

    Incluindo programinha básico de saque em terminal eletrônico.

---
+++ ContaBanco/src/CaixaEletronico.java
@@ -0,0 +1,17 @@
+public class CaixaEletronico {
+    public static void main (String[] args) {
+
+        double saldo = 25.0;
+        double valorSolicitado = 29.0;
+        
+        // Se tem saldo na conta
+        if (saldo >= valorSolicitado){
+         saldo -= valorSolicitado;
+         System.out.println("Saque realizado com sucesso. Saque de R$ " + valorSolicitado + ". " + "Seu saldo atual é de R$: " + saldo + ".");
+        }
+        // Não tem saldo na conta
+        else 
+        System.out.println("Saldo solicitado maior que seu saldo atual. Seu saldo é de R$ " + saldo);
+ 
+     }
+ }
