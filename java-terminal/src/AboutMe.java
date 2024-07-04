aa3181ca1a46d0ead8fce0f275d3f709021a9311
angelica.kadja <angelicakadja@gmail.com>
2024-07-04 19:57:50 -0300

    Criando projetinhos com exceções.

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -3,12 +3,18 @@ public class AboutMe {
 
         String nome = args [0];
         String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        int idade = Integer.parseInt(args[2]);
+        double altura = Double.parseDouble(args[3]);
 
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + " cm ");
 
+
+        /*Onde tem Integer.valueOf(args[2])
+         * Ao invés de usar valueOf para converter String em número, podemos usar parseInt
+         * Onde tem Double.valueOf(args[3])
+         * Ao invés de usar Double.valueOf para converter qualquer parâmetro para double, use o parseDouble
+         */
     }
 }
--- java-terminal/src/AboutMeInteracaoUsuario.java
+++ java-terminal/src/AboutMeInteracaoUsuario.java
@@ -3,25 +3,31 @@ import java.util.Scanner;
 
 public class AboutMeInteracaoUsuario {
     public static void main(String[] args) {
+
         //criando o objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
-        
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
-
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
-        
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
-
-
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + " cm ");
-
+        try {
+            try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+                System.out.println("Digite seu nome");
+                String nome = scanner.next();
+                
+                System.out.println("Digite seu sobrenome");
+                String sobrenome = scanner.next();
+                
+                System.out.println("Digite sua idade");
+                int idade = scanner.nextInt();
+                
+                System.out.println("Digite sua altura");
+                double altura = scanner.nextDouble();
+                
+                //imprimindo os dados obtidos pelo usuario cm exceções de conversões
+                System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+                System.out.println("Tenho " + idade + " anos ");
+                System.out.println("Minha altura é " + altura + " cm ");
+            }
+        }
+        catch (java.util.InputMismatchException e) {
+            System.err.println("Os campos idade e altura precisam ser numéricos e o campo altura com ponto");
+        }
     }
 }
